cb71de310b16d08f1dfb8bd1287bc44bf806e9ba
skyeome <tjru7230@naver.com>
2023-12-06 16:09:09 +0900

    Style: 지역별 상품 페이지 반응형

--- src/components/regionProd/RegionProdCategories.tsx
+++ src/components/regionProd/RegionProdCategories.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { KeyboardArrowRight } from '@mui/icons-material';
-import styles from './RegionProd.module.css';
 import { RegionProdProps } from 'types/RegionProd.type';
+import styles from './RegionProd.module.css';
 
 function RegionProdCategories({ region }: RegionProdProps) {
 	return (
 		<div className="flex gap-2 py-4">
 			<div className={styles.linkItem}>
 				<Link to={`/hotel/${region}`}>
-					호텔 <KeyboardArrowRight sx={{ verticalAlign: 'bottom' }} />
+					호텔 <KeyboardArrowRight />
 				</Link>
 			</div>
 			<div className={styles.linkItem}>
 				<Link to={`/resort/${region}`}>
-					리조트 <KeyboardArrowRight sx={{ verticalAlign: 'bottom' }} />
+					리조트 <KeyboardArrowRight />
 				</Link>
 			</div>
 			<div className={styles.linkItem}>
 				<Link to={`/pension/${region}`}>
-					펜션 <KeyboardArrowRight sx={{ verticalAlign: 'bottom' }} />
+					펜션 <KeyboardArrowRight />
 				</Link>
 			</div>
 			<div className={styles.linkItem}>
 				<Link to={`/pool_villa/${region}`}>
-					풀빌라 <KeyboardArrowRight sx={{ verticalAlign: 'bottom' }} />
+					풀빌라 <KeyboardArrowRight />
 				</Link>
 			</div>
 		</div>
