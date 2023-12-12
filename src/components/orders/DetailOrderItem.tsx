f71626ec29dcf07fcfb03d0544fdf0a4b31518ed
hhjs2 <0092h@naver.com>
2023-12-13 00:14:39 +0900

    feat:코드리뷰 리팩토링

--- src/components/orders/DetailOrderItem.tsx
+++ src/components/orders/DetailOrderItem.tsx
@@ -13,49 +13,44 @@ const checkOutDate = useRecoilValue(checkOutDateState);
 const DetailOrderItem = () => {
 	return (
 		<>
-			{' '}
 			{orderItem?.id && (
-				<>
-					<div key={orderItem.id}>
-						<div className="my-4">
-							<div className="font-semibold ">
-								{orderItem.accommodationName}
-							</div>
+				<div key={orderItem.id}>
+					<div className="my-4">
+						<div className="font-semibold ">{orderItem.accommodationName}</div>
+						<div className="font-semibold text-content ">
+							{orderItem.roomTypeName}
+						</div>
+					</div>
+					<div className="flex ">
+						<div className="flex flex-col w-1/2">
+							<div className="text-xxsm text-textGray">체크인</div>
 							<div className="font-semibold text-content ">
-								{orderItem.roomTypeName}
+								<>{checkInDate}(화)</>
 							</div>
+							<div className="text-sm">15: 00</div>
 						</div>
-						<div className="flex ">
-							<div className="flex flex-col w-1/2">
-								<div className="text-xxsm text-textGray">체크인</div>
-								<div className="font-semibold text-content ">
-									<>{checkInDate}(화)</>
-								</div>
-								<div className="text-sm">15: 00</div>
+						<div className="flex flex-col w-1/2 ">
+							<div className="text-xxsm text-textGray">체크아웃</div>
+							<div className="font-semibold text-content ">
+								<>{checkOutDate} (금)</>
 							</div>
-							<div className="flex flex-col w-1/2 ">
-								<div className="text-xxsm text-textGray">체크아웃</div>
-								<div className="font-semibold text-content ">
-									<>{checkOutDate} (금)</>
-								</div>
-								<div className="text-sm">19: 00</div>
-							</div>
+							<div className="text-sm">19: 00</div>
 						</div>
-						<div className="text-xxsm text-textGray flex items-center py-2">
-							<PersonOutlineRoundedIcon sx={{ fontSize: '1rem' }} /> 기준 2명 /
-							최대
-							{orderItem.capacity}명
-						</div>
-						<div>
-							<div className="flex justify-end items-center content-center">
-								<div className="text-sm text-textGray pr-1">연박 / 3박</div>
-								<div className="font-semibold text-content">
-									{formatNumberWithCommas(orderItem.price)} 원
-								</div>
+					</div>
+					<div className="text-xxsm text-textGray flex items-center py-2">
+						<PersonOutlineRoundedIcon sx={{ fontSize: '1rem' }} /> 기준 2명 /
+						최대
+						{orderItem.capacity}명
+					</div>
+					<div>
+						<div className="flex justify-end items-center content-center">
+							<div className="text-sm text-textGray pr-1">연박 / 3박</div>
+							<div className="font-semibold text-content">
+								{formatNumberWithCommas(orderItem.price)} 원
 							</div>
 						</div>
 					</div>
-				</>
+				</div>
 			)}
 		</>
 	);
